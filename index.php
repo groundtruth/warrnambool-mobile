47ac65b3b2cb3e1e761116b803793fdbcdbbd6e8
Chris Berkhout <chrisberkhout@gmail.com>
2013-05-14 18:14:45 +1000

    Revert "Removed pit add stuff from interface."
    This reverts commit 8bf173bbdbda35e54bf2cd161b072eab4f910296.

--- index.php
+++ index.php
@@ -127,12 +127,27 @@
                             pack: "center"
                         },
                         items: [{
+								iconCls: "add",
+								iconMask: true,
+								handler: function() {
+									if (!app.capturePitFormPopupPanel ) {
+										app.capturePitFormPopupPanel = new App.CapturePitFormPopupPanel({
+											map: map
+										});
+									}
+									else
+									{
+										// Updating the lat / lon values in the existing form
+										app.capturePitFormPopupPanel.formContainer.setValues({
+											'lat':map.getCenter().transform(sm,gg).lat,
+											'lon':map.getCenter().transform(sm,gg).lon
+										});
+									}
+									app.capturePitFormPopupPanel.show('pop');
+								}
+                        		},{
 								xtype:"toolbartext",
-								html:'',
-								width:'auto'
-					},{
-								xtype:"toolbartext",
-								html:'',
+								html:'Pit',
 								width:'auto'
 					},{
 								xtype: "spacer"
